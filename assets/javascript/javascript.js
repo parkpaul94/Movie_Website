6796a8c82e079f9787caa06adbd330aded4e254d
Paul Park <park.paul.94@gmail.com>
2018-03-16 02:43:49 -0400

    finished product, still trying to work out if I can append random movie posters on the main screen, trying to figure out if I should make the search results appear on a new page. Trying to see if I can make it so you can select by genres, actors, directors etc

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
-    // $('.navsearch').hide();
+  $('#searchedFor').hide();
     $('#movie_search').on('submit', function(event) {
-        $('.searchcc').hide();
-        // $('.navsearch').show();
+      event.preventDefault();
         var searched = ($('#searchText').val());
         MovieSearch(searched);
-        event.preventDefault();
+        $('#searchText').val('');
+
     });
 });
 
-
-
-
 var MovieSearch = function(searched) {
+  $('#searchedFor').show();
     axios.get("https://www.omdbapi.com/?apikey=trilogy&s=" + searched)
     .then(function(response) {
     console.log(response);
@@ -23,8 +21,8 @@ var MovieSearch = function(searched) {
         output += `
         <div class="movies">
           <div class="center">
-            <img src="${movie.Poster}">
-            <h5>${movie.Title}</h5>
+            <img class="posterimage" src="${movie.Poster}">
+            <h5 class="postername">${movie.Title}</h5>
             <a onclick="movieSelected('${movie.imdbID}')" class="moreinfo" href="#">More Info</a>
           </div>
         </div>
@@ -32,8 +30,94 @@ var MovieSearch = function(searched) {
     });
 
     $('#movie-list').html(output);
+    $('#searchedFor').html('<h2 class ="searchtext">Search Results for:<br>' + searched);
+    // sessionStorage.setItem('searchedM', searched);
+    console.log(searched);
   })
-  .catch((err) => {
-    console.log(err);
-  });
 };   
+
+// function searchedMovie(searched) {
+//     var mID = sessionStorage.getItem('searchedM')
+//     axios.get("https://www.omdbapi.com/?apikey=trilogy&t=" + searched)
+//     .then(function(response) {
+//     console.log(response);
+//     });
+// }
+
+function movieSelected(Info) {
+    sessionStorage.setItem('mID', Info);
+    window.location = 'movie.html';
+    return false;
+}
+
+
+function movieChosen() {
+    var mID = sessionStorage.getItem('mID')
+    axios.get("https://www.omdbapi.com/?apikey=trilogy&i=" + mID)
+    .then(function(response) {
+    console.log(response);
+    let movie = response.data;
+    let output = `
+    <div class="row">
+    <div class="column">
+    <div class="postercard">
+      <img src="${movie.Poster}" class="thumbnail">
+      </div>
+    </div>
+    <div class="infocontent">
+      <h2 class="movietitle">${movie.Title}</h2>
+      <ul class="listbox">
+        <li class="list"><strong>Genre:</strong> ${movie.Genre}</li>
+        <li class="list"><strong>Released:</strong> ${movie.Released}</li>
+        <li class="list"><strong>Rated:</strong> ${movie.Rated}</li>
+        <li class="list"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+        <li class="list"><strong>Director:</strong> ${movie.Director}</li>
+        <li class="list"><strong>Writer:</strong> ${movie.Writer}</li>
+        <li class="list"><strong>Actors:</strong> ${movie.Actors}</li>
+      </ul>
+    </div>
+  </div>
+  <div class="row">
+    <div class="row-container">
+      <h2 class="contentfont">Plot</h2>
+      <div class ="plottext">
+      ${movie.Plot}
+
+      <hr class="divide">
+      <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="viewmore ">IMDB</a>
+      <a href="https://www.google.com/search?q=${movie.Title}+trailer" target="_blank" class="trailer ">Find Trailer</a>
+    </div>
+    </div>
+  </div>
+  `;
+  $('#movie').html(output);
+    }); 
+    
+}
+
+// var slideshow = function() {
+//   $('#searchedFor').show();
+//     axios.get("http://img.omdbapi.com/?apikey=trilogy&")
+//     .then(function(response) {
+//     console.log(response);
+//     let movies = response.data.Search;
+//     let output = '';
+//     console.log(response.data.Search);
+//     $.each(movies, function(index, movie) {
+//         output += `
+//         <div class="movies">
+//           <div class="center">
+//             <img class="posterimage" src="${movie.Poster}">
+//             <h5 class="postername">${movie.Title}</h5>
+//             <a onclick="movieSelected('${movie.imdbID}')" class="moreinfo" href="#">More Info</a>
+//           </div>
+//         </div>
+//       `;
+//     });
+
+//     $('#movie-list').html(output);
+//     $('#searchedFor').html('<h2 class ="searchtext">Search Results for:<br>' + searched);
+//     // sessionStorage.setItem('searchedM', searched);
+//     console.log(searched);
+//   })
+// };   
